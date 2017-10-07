d4ab29fc76af621d9a85f6fce1073d5a8e4ceafa
Prabodh Agarwal <prabodh1194@gmail.com>
2017-10-07 20:20:06 +0530

    Make server core module with pure functions
    Vote and revote functions now return a set of objects structured same as
    object state itself, but actually just carry changes and are merged with
    state object in reducer module.

--- src/server/reducer.js
+++ src/server/reducer.js
@@ -5,7 +5,39 @@ function reducer(state = {}, action) {
     case 'init_state':
       return core.initState(action.payload);
     case 'vote':
-      return core.vote(state, action.payload, action.clientId);
+      returnSet = core.vote(state, action.payload, action.clientId);
+
+      /** Some debugging statements to understand the working of changeset
+      console.log(returnSet);
+      console.log(state);
+      console.log("==========");
+      */
+
+      // merge changes from returnSet into state
+      Object.keys(returnSet.topics).map(key => {
+        state.topics[key].votes += returnSet.topics[key].votes;
+      });
+      /** Some debugging statements to understand the working of changeset
+      console.log(state);
+      console.log("==========");
+      */
+      returnSet.voters.forEach(voter => {
+        voterId = voter.id;
+        const stateVoter = state.voters.find(stateVoter => stateVoter.id === voterId);
+
+        if (!stateVoter) {
+          state.voters.push(voter)
+        }
+        else {
+          stateVoter.vote = voter.vote;
+        }
+
+      });
+      /** Some debugging statements to understand the working of changeset
+      console.log(state);
+      console.log("==========\n\n");
+      */
+      return state;
     default:
       return state;
   }
