e21b43afff70fbe04a346435ffe09569b9e7b0db
Evgeny Rodin <eugene.rodin1@gmail.com>
2019-08-25 15:03:34 +0300

    chore(scripts): optimized version

--- common/scripts/use_config_files.sh
+++ common/scripts/use_config_files.sh
@@ -6,28 +6,23 @@ parent_path=$(
 )
 "$parent_path/check_sign_key.sh"
 
-function encode_file() {
-  openssl enc -in $1 -md md5 -out $2 -e -des-ede3-cbc -pass pass:$SIGN_KEY
+function enc_file() {
+  openssl enc -in $1 -md md5 -out $2 $param -des-ede3-cbc -pass pass:$SIGN_KEY
 }
 
-function decode_file() {
-  openssl enc -in $1 -md md5 -out $2 -d -des-ede3-cbc -pass pass:$SIGN_KEY
-
-}
-
-args=("$@")
 param="$1"
+args=("$@")
 files=("${args[@]:1}")
 
 case $param in
 -d)
   for paths in "${files[@]}"; do
-    decode_file "${paths%%:*}" "${paths#*:}"
+    enc_file "${paths%%:*}" "${paths#*:}"
   done
   ;;
 -e)
   for paths in "${files[@]}"; do
-    encode_file "${paths#*:}" "${paths%%:*}"
+    enc_file "${paths#*:}" "${paths%%:*}"
   done
   ;;
 esac
