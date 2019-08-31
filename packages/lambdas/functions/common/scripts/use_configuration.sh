43e30d85caae20b8fd5eb661d3049b237226279c
Evgeny Rodin <eugene.rodin1@gmail.com>
2019-08-31 14:49:34 +0300

    chore(config-files): changed files type

--- packages/lambdas/functions/common/scripts/use_configuration.sh
+++ packages/lambdas/functions/common/scripts/use_configuration.sh
@@ -3,8 +3,8 @@
 package=$PWD
 
 files=(
-  "$package/common/configuration/enc-app-keys-prod:$package/voteit-app-keys.json"
-  "$package/common/configuration/enc-app-keys-stg:$package/voteit-app-stg-keys.json"
+  "$package/common/configuration/app-keys-prod.enc:$package/voteit-app-keys.json"
+  "$package/common/configuration/app-keys-stg.enc:$package/voteit-app-stg-keys.json"
 )
 
 "../../../common/scripts/use_config_files.sh" -d "${files[@]}"
--- packages/web/common/scripts/use_configuration.sh
+++ packages/web/common/scripts/use_configuration.sh
@@ -3,11 +3,11 @@
 package=$PWD
 
 files=(
-  "$package/common/configuration/enc-service-account:$package/serviceAccount.json"
-  "$package/common/configuration/enc-cypress-env:$package/cypress.env.json"
-  "$package/common/configuration/enc-firebase-config:$package/cypress/support/firebase.config.json"
-  "$package/common/configuration/enc-cypress-config:$package/cypress/config.json"
-  "$package/common/configuration/enc-cypress-config:$package/cypress/config.json"
+  "$package/common/configuration/service-account.enc:$package/serviceAccount.json"
+  "$package/common/configuration/cypress-env.enc:$package/cypress.env.json"
+  "$package/common/configuration/firebase-config.enc:$package/cypress/support/firebase.config.json"
+  "$package/common/configuration/cypress-config.enc:$package/cypress/config.json"
+  "$package/common/configuration/cypress-config.enc:$package/cypress/config.json"
 )
 
 "../../common/scripts/use_config_files.sh" -d "${files[@]}"
